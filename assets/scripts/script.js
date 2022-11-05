407725aee34ba48259628496deba3b87b9d09b14
lesley-byte <phosphorescence3@hotmail.com>
2022-11-05 14:29:29 -0400

    adding starter code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,30 @@
 // This will all be JQuery
+class julianDay {
+  constructor(dayOfYear) {
+    this.dayOfYear = dayOfYear;
+    for (let i = 0; i < 24; i++) {
+      let timeOfDay = i;
+      this[timeOfDay] = "";
+    }
+  }
+}
+
+class nonLeapYear {
+  constructor(specYear) {
+    for (let i = 1; i < 366; i++) {
+      let dayOfSpecYear = i;
+      this[dayOfSpecYear] = new julianDay(i);
+    }
+  }
+}
+class leapYear {
+  constructor(otherYear) {
+    for (let i = 1; i < 367; i++) {
+      let dayOfOtherYear = i;
+      this[dayOfOtherYear] = new julianDay(i);
+    }
+  }
+}
+var thursJan10 = new nonLeapYear("2022")[10];
+thursJan10[10] = "doctor's Appointment";
+console.log(thursJan10);
