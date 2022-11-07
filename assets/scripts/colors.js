aa6f579cb011bce44f2dfd904850c87f73de1c27
lesley-byte <phosphorescence3@hotmail.com>
2022-11-06 20:02:21 -0500

    trying to fix placeholders

--- assets/scripts/colors.js
+++ assets/scripts/colors.js
@@ -1,25 +1,4 @@
-var now = dayjs(); // real current time
-var now0 = now; // time that starts at real but is modified
-var today = now.date(); // real current day
-var today0 = now0.date(); // day that starts at real but is modified
-var thisDay = now.day(); // real current day of week
-var thisDay0 = now0.day(); // day of week that starts at real but is modified
-var thisHour = now.hour(); // real current hour
-var thisHour0 = now0.hour(); // hour that starts at real but is modified
-
-function logger() {
-  console.log(localStorage.getItem("input1"));
-  console.log(localStorage.getItem("input2"));
-  console.log(localStorage.getItem("input3"));
-  console.log(localStorage.getItem("input4"));
-  console.log(localStorage.getItem("input5"));
-  console.log(localStorage.getItem("input6"));
-  console.log(localStorage.getItem("input7"));
-  console.log(localStorage.getItem("input8"));
-  console.log(localStorage.getItem("input9"));
-}
-
-function setColors() {
+export function setColors() {
   if (today0 > today) {
     $("#input9").css({ "background-color": "green" });
     $("#input8").css({ "background-color": "green" });
@@ -30,7 +9,7 @@ function setColors() {
     $("#input3").css({ "background-color": "green" });
     $("#input2").css({ "background-color": "green" });
     $("#input1").css({ "background-color": "green" });
-  } else if (today0 <= today) {
+  } else if (today0 < today) {
     $("#input9").css({ "background-color": "dimgrey" });
     $("#input8").css({ "background-color": "dimgrey" });
     $("#input7").css({ "background-color": "dimgrey" });
@@ -41,8 +20,7 @@ function setColors() {
     $("#input2").css({ "background-color": "dimgrey" });
     $("#input1").css({ "background-color": "dimgrey" });
     //if hour is greater than 5pm, then set background color of .input-group to grey
-  }
-  if (thisHour0 > 17) {
+  } else if (thisHour0 > 17) {
     // id input1 through input9 have background color of dark grey
     $(".input-group").css("background-color", "#d9e9e8");
   } else if (thisHour0 <= 16 && thisHour0 > 15) {
@@ -117,102 +95,3 @@ function setColors() {
     $("#input1").css({ "background-color": "green" });
   }
 }
-// This will all be JQuery
-class julianDay {
-  constructor(dayOfYear) {
-    this.dayOfYear = dayOfYear;
-    for (let i = 9; i < 18; i++) {
-      let timeOfDay = i;
-      this[timeOfDay] = "";
-    }
-  }
-}
-// add event listener to the button0 that will increase now by 1 day
-$("#increment-btn").click(function () {
-  now0 = now0.add(1, "day");
-  today0 = now0.date();
-  thisDay0 = now0.day();
-  thisHour0 = now0.hour();
-  // update the date on the page
-  $("#currentDay").text(now0.format("dddd, MMMM D, YYYY"));
-  setColors();
-});
-
-$("#decrement-btn").click(function () {
-  now0 = now0.subtract(1, "day");
-  today0 = now0.date();
-  thisDay0 = now0.day();
-  thisHour0 = now0.hour();
-  // update the date on the page
-  $("#currentDay").text(now0.format("dddd, MMMM D, YYYY"));
-  setColors();
-});
-
-$("#currentDay").text("Today is " + now.format("dddd, MMMM D, YYYY"));
-setColors();
-logger();
-$("#input1").attr("placeholder", localStorage.getItem("input1"));
-// JQuery event listener for the the button-addon1 to write the text in input1 to the label for input1
-$("#button-addon1").click(function () {
-  // get the text for input1 from local storage
-  $("#basic-addon1").text(localStorage.getItem("input1"));
-  $("#basic-addon1").attr("placeholder", localStorage.getItem("input1"));
-  let input1 = $("#input1").val();
-  localStorage.setItem("input1", input1);
-});
-
-// JQuery event listener for the the button-addon2 to write the text in input2 to the label for input2
-$("#button-addon2").click(function () {
-  let input2 = $("#input2").val();
-  $("#label2").text(input2);
-  localStorage.setItem("input2", input2);
-});
-
-// JQuery event listener for the the button-addon3 to write the text in input3 to the label for input3
-$("#button-addon3").click(function () {
-  let input3 = $("#input3").val();
-  $("#label3").text(input3);
-  localStorage.setItem("input3", input3);
-});
-
-// JQuery event listener for the the button-addon4 to write the text in input4 to the label for input4
-$("#button-addon4").click(function () {
-  let input4 = $("#input4").val();
-  $("#label4").text(input4);
-  localStorage.setItem("input4", input4);
-});
-
-// JQuery event listener for the the button-addon5 to write the text in input5 to the label for input5
-$("#button-addon5").click(function () {
-  let input5 = $("#input5").val();
-  $("#label5").text(input5);
-  localStorage.setItem("input5", input5);
-});
-
-// JQuery event listener for the the button-addon6 to write the text in input6 to the label for input6
-$("#button-addon6").click(function () {
-  let input6 = $("#input6").val();
-  $("#label6").text(input6);
-  localStorage.setItem("input6", input6);
-});
-
-// JQuery event listener for the the button-addon7 to write the text in input7 to the label for input7
-$("#button-addon7").click(function () {
-  let input7 = $("#input7").val();
-  $("#label7").text(input7);
-  localStorage.setItem("input7", input7);
-});
-
-// JQuery event listener for the the button-addon8 to write the text in input8 to the label for input8
-$("#button-addon8").click(function () {
-  let input8 = $("#input8").val();
-  $("#label8").text(input8);
-  localStorage.setItem("input8", input8);
-});
-
-// JQuery event listener for the the button-addon9 to write the text in input9 to the label for input9
-$("#button-addon9").click(function () {
-  let input9 = $("#input9").val();
-  $("#label9").text(input9);
-  localStorage.setItem("input9", input9);
-});
